e1e0f7d8dc5a9610cd0f50626b5c3b63c8eeede1
ayush-bug <ayushkushwaharxl2007@gmail.com>
2026-08-19 14:43:30 +0530

    making menu items

---
+++ src/menu.js
@@ -0,0 +1,12 @@
+import '/src/menu.css'
+document.querySelector("#app").innerHTML = ` 
+<h1> menu items check </h1>
+`;
+document.querySelector("#app").innerHTML = ` 
+<div class="main-container"> 
+  <header>
+ <div> Menu</div>
+ <input type="search" placeholder="Search itmes" class="search-items">
+  </header>
+</div>
+`;
